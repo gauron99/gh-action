622b3a62444ddde68ddab8687e13374385551745
David Fridrich <fridrich.david19@gmail.com>
2024-12-02 13:55:52 +0100

    init
    Signed-off-by: David Fridrich <fridrich.david19@gmail.com>

---
+++ index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // Fetch value of inputs specified in action.yml
+  const nameToGreet = core.getInput('who-to-greet');
+  const version = core.getInput('version');
+
+  console.log("Hey there ${nameToGreet}")
+  console.log("I see you're using ${version} version, nice!")
+
+  // save time of greeting
+  const time = (new Date().toTimeString());
+  core.setOutput("time",time)
+
+} catch (error) {
+  core.setFailed(error.message)
+}
