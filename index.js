a7d42089b5e8966454d2be5ef1e22352ee053d99
David Fridrich <fridrich.david19@gmail.com>
2024-12-09 15:37:02 +0100

    fix
    Signed-off-by: David Fridrich <fridrich.david19@gmail.com>

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+	// default values
+	const default_version = "v1.16"
+	const default_os = "func_linux_amd64"
+	const default_url = `https://github.com/knative/func/releases/download/knative-${default_version}/${default_os}`
+
   // Fetch value of inputs specified in action.yml
   const nameToGreet = core.getInput('who-to-greet');
   const version = core.getInput('version');
+  console.log(`> version is; ${version}`)
+  
+ 	var url_default = `https://github.com/knative/func/releases/download/knative-${version}/func_linux_amd64`
+	
+ 	// do this after: curl -SLO $program_url && mv func_linux_amd64 f && chmod +x f
+
 
   console.log(`Hey there ${nameToGreet}`);
   console.log(`I see you're using ${version} version, nice!`);
