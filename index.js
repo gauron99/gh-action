1345f02a6d40cd34b029803db88abfa5c493a370
David Fridrich <fridrich.david19@gmail.com>
2024-12-09 17:40:52 +0100

    fix
    Signed-off-by: David Fridrich <fridrich.david19@gmail.com>

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+
+const os_values = ['func_linux_amd64'];
+
+function isVersionValid(version){
+  const versRegEx = /^(v?\d+\.\d+)(\.d+)?$/;
+  return versRegEx.test(version);
+}
+
 try {
 	// default values
-	const default_version = "v1.16"
-	const default_os = "func_linux_amd64"
-	const default_url = `https://github.com/knative/func/releases/download/knative-${default_version}/${default_os}`
+	// const default_url = `https://github.com/knative/func/releases/download/knative-${default_version}/${default_os}`
 
   // Fetch value of inputs specified in action.yml
-  const nameToGreet = core.getInput('who-to-greet');
+  const os = core.getInput('os');
   const version = core.getInput('version');
-  console.log(`> version is; ${version}`)
   
- 	var url_default = `https://github.com/knative/func/releases/download/knative-${version}/func_linux_amd64`
+  // validate
+  if (!isVersionValid(version)){
+    core.setFailed('Invalid version format. Expected format: "1.16" or "v1.16"');
+    return;
+  }
+  if (!(isOsValid(os))){
+    core.setFailed(`Invalid OS format. Expected values are: ${os_values}`);
+    return;
+  }
+
+ 	var url = `https://github.com/knative/func/releases/download/knative-${version}/${os}`;
+  console.log(`FINAL URL IS :${url}`) 
 	
  	// do this after: curl -SLO $program_url && mv func_linux_amd64 f && chmod +x f
-
-
-  console.log(`Hey there ${nameToGreet}`);
-  console.log(`I see you're using ${version} version, nice!`);
 
   // save time of greeting
   const time = (new Date().toTimeString());
