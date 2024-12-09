9cfdc9a39efdfa2e5c2092c211844c4c77e3adda
David Fridrich <fridrich.david19@gmail.com>
2024-12-09 18:21:32 +0100

    fix
    Signed-off-by: David Fridrich <fridrich.david19@gmail.com>

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ const github = require('@actions/github');
 
 const os_values = ['func_linux_amd64'];
 
-function isVersionValid(version){
-  const versRegEx = /^(v?\d+\.\d+)(\.d+)?$/;
-  return versRegEx.test(version);
+// smartVersionParse will check validity of given version and fill in the parts
+// to make it correct if possible.
+// Ex.: '1.16' or 'v1.16' will return 'v1.16.0'
+function smartVersionParse(version){
+  var versionRegex = /^(v?\d+\.\d+)(\.d+)?$/;
+  if (!versionRegex.test(version)){
+    core.setFailed('Invalid version format. Expected format: "1.16[.X]" or "v1.16[.X]"');
+    return;
+  }
+  versionRegex = /^(v?)(?<major>\d+)\.(?<minor>\d+)$/;
+  if (version.match(versionRegex)) {
+    const prefix = 'v';
+    return `${prefix}${match.groups.major}.${match.groups.minor}.0`;
+  } else {
+    return version;
+  }
 }
 
-function isOsValid(os){
+// smartOsParser will check validity of the given os and try to match its
+// actuall binary name. Ex.: giving 'linux' might rewrite to 'func_linux_amd64'
+function smartOsParse(os){
+  //TODO: change this to match this functions description
   return os_values.includes(os)
 }
 
@@ -21,12 +37,10 @@ try {
   const os = core.getInput('os');
   const version = core.getInput('version');
   
-  // validate
-  if (!isVersionValid(version)){
-    core.setFailed('Invalid version format. Expected format: "1.16" or "v1.16"');
-    return;
-  }
-  if (!(isOsValid(os))){
+  version = smartVersionParse(version)
+  // os = smartOsParse(os) // TODO: this function needs to be updated first
+
+  if (!(smartOsParse(os))){
     core.setFailed(`Invalid OS format. Expected values are: ${os_values}`);
     return;
   }
